f8e578a851ffc65b9cf10cd59c754737d9fbab05
hannahsaintonge <hannahsaintonge@gmail.com>
2018-05-30 16:00:59 -0400

    added another http request

--- service.js
+++ service.js
@@ -4,7 +4,8 @@ function MovieService($http) {
     const getMovie = () => {
         return $http ({
             method: "GET",
-            url: "https://api.themoviedb.org/3/movie/550?api_key=cab156d1e88036f2189b1c07c86428d5"
+            // url: "https://api.themoviedb.org/3/movie/550?api_key=cab156d1e88036f2189b1c07c86428d5"
+               url: "https://api.themoviedb.org/3/movie/popular?api_key=cab156d1e88036f2189b1c07c86428d5&language=en-US&page=1"
         }).then((response) => {
             movieData = response;
             return movieData;
